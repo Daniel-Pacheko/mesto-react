0716d1e62ce985d7cb092266b852892f03cd3ca0
Daniel-Pacheko <87881087+Daniel-Pacheko@users.noreply.github.com>
2022-06-22 01:13:43 +0300

    Delete mesto-react directory

--- mesto-react/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer className="footer">
-            <p className="footer__author-name">© 2020. Daniel Pacheco</p>
-        </footer>
-    );
-}
-
-export default Footer;
